4c4ec8d6bc9501a5b805332842e1d6590b54210c
visualstudio <visualstudio@122-PC35>
2019-03-27 08:17:12 -0400

    second commit

--- Assignment 3 CSD-3464_4/Program.cs
+++ Assignment 3 CSD-3464_4/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Assignment_3_CSD_3464_4
 {
+    public delegate void Sort_DataStructure();
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Student a = new Student();
+            a.Announcement();
+
         }
     }
 }
